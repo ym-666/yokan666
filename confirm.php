c738c8568f5cd1dbbbb6817778b86004ce1747a1
ym-666 <xmayoshix@gmail.com>
2024-06-07 01:57:56 +0900

    Add files via upload

---
+++ confirm.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YOKAN　大阪北浜の老舗洋菓子店</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="ue">
+            <div class="logo"><img src="img/logo.png" alt="GOKAN 五感旬菓子"></div>
+            <div class="online">
+                <a href="https://shop.patisserie-gokan.co.jp/">
+                    <img src="img/migiueon.png" alt="GOKAN オンラインショップ">
+                </a></div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.html">五感の想い</a></li>
+                <li><a href="goods.html">商品紹介</a></li>
+                <li><a href="store.html">店舗案内</a></li>
+                <li><a href="company.html">会社概要</a></li>
+                <li><a href="form.html">お問い合わせ</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div id="topimg">
+            <a href="goods.html"><img src="img/topw.jpg" alt="季節のお菓子特集"></a>
+        </div>
+        <div class="container">
+            <div class="left">
+                <img src="img/tit.jpg" alt="五感の想い">
+                <img src="img/making.png" alt="日本の農業を活かし、日本人の心に響く洋菓子をつくる">
+                <a href="goods.html"><img src="img/topcake.jpg" alt="洋菓子"></a>
+                <p>日本人にとって、毎日食べるお米はかけがえのないものです。
+                    「米」という文字は、収穫までに「八十八」もの手間をかけるからだといわれるように、農家
+                    さんが手塩にかけて育てたものです。
+                    「米一粒でも残すな。食べ物を粗末にするな」
+                    と、昔から親は子を厳しく躾けてきました。そこに込められた、人や自然への感謝と食べ物を
+                    大切にするという心は、時代が変わっても忘れてはならないことです。
+
+                    「お世話になった方への恩を忘れるな」
+                    人の縁を大切に、出会いに感謝し、そのご恩に報いるためにも、五感はできうる限りお菓子づ
+                    くりで社会に貢献したいと考えています。
+
+                    日本の作物の優れた品質や安全性は、技術力に加えて作物への深い愛情によって支えられてい
+                    ます。五感もまた、「美味しくて喜ばれるお菓子をつくろう」と日々努力をしています。同じ
+                    時代を生きる農業に携わる人々とともに、自然の恵みを活かし、「日本人の心に響くお菓子」
+                    を心を込めてつくります。</p>
+            </div>
+            <div class="right">
+                <div class="rr"><a href="https://shop.patisserie-gokan.co.jp/"><img src="img/online.jpg"
+                            alt="オンラインショップ"></a></div>
+                <div class="rrr"><a href="https://www.patisserie-gokan.co.jp/season/"><img src="img/banne.jpg"
+                            alt="限定菓子特集"></a></div>
+                <a href="https://www.patisserie-gokan.co.jp/recruit/"><img src="img/boshu.jpg" alt="アルバイト募集中"></a>
+                <p>五感ではアルバイトを募集しています。</p>
+                <p>11：00～19：00の間で4時間から</p>
+                <p>時給1100円～</p>
+                <p>詳しくは06-1111-1111まで</p>
+                <p>担当：田村</p>
+            </div>
+        </div>
+    </main>
+
+
+</body>
+<footer>
+    <ul>
+        <li><a href="index.html">HOME</a></li>
+        <li><a href="index.html">五感の想い</a></li>
+        <li><a href="goods.html">商品紹介</a></li>
+        <li><a href="store.html">店舗案内</a></li>
+        <li><a href="company.html">会社概要</a></li>
+        <li><a href="form.html">お問い合わせ</a></li>
+    </ul>
+    <p>Copyright © patisserie de samourai GOKAN. All rights reserved.</p>
+</footer>
+
+</html>

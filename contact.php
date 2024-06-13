011c6c3e276bcac53ba6f1bcbc3d5eb682fe0ef2
ym-666 <xmayoshix@gmail.com>
2024-06-13 23:17:20 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 if($_POST) {
   echo $_POST['お名前'] . '<br>';
   echo $_POST['電話番号'] . '<br>';
-  echo $_POST['Email'] . '<br>';
+  echo $_POST['Mail（半角）'] . '<br>';
   echo $_POST['お問い合わせ内容'];
 } else {
   echo "HTMLからのPOST送信受信に失敗しました";

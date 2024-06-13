5097ccaf717bed258f15dbcc1cb9df6ba35d7239
ym-666 <xmayoshix@gmail.com>
2024-06-13 23:49:31 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 if($_POST) {
   echo $_POST['お名前'] . '<br>';
-  echo $_POST['電話番号'] . '<br>';
+  echo $_POST['電話番号（半角）'] . '<br>';
   echo $_POST['Mail（半角）'] . '<br>';
   echo $_POST['お問い合わせ内容'];
 } else {

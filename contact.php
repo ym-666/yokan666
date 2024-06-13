4063ef1ba5f66ea5c5072a397b0952322bcfce9c
ym-666 <xmayoshix@gmail.com>
2024-06-13 23:05:20 +0900

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,30 +1,10 @@
-<?php 
-　if ($_SERVER["REQUEST_METHOD"] == "POST") { 
-　　$name = $_POST["name"];
-　　$email = $_POST["email"];
-　　$message = $_POST["message"]; 
-　} else { 
-　　header("Location: contact.php"); 
-　} 
-?>
-<html>
-　<head>
-　　<title>お問い合わせフォーム - 確認画面</title>
-　</head>
-　<body>
-　　<h1>お問い合わせフォーム - 確認画面</h1>
-　　<p>以下の内容で送信します。よろしいですか？</p>
-　　<table>
-　　　<tr><td>名前：</td><td><?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?></td></tr>
-　　　<tr><td>メールアドレス：</td><td><?php echo htmlspecialchars($email, ENT_QUOTES, 'UTF-8'); ?></td></tr>
-　　　<tr><td>お問い合わせ内容：</td><td><?php echo htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></td></tr>
-　　</table>
-　　<form method="post" action="send.php">
-　　　<input type="hidden" name="name" value="<?php echo $name; ?>">
-　　　<input type="hidden" name="email" value="<?php echo $email; ?>">
-　　　<input type="hidden" name="message" value="<?php echo $message; ?>">
-　　　<input type="submit" value="送信">
-　　　<button type="button" onclick="history.back()">戻る</button>
-　　</form>
-　</body>
-</html>
+<?php
+
+if($_POST) {
+  echo $_POST['お名前'] . '<br>';
+  echo $_POST['電話番号'] . '<br>';
+  echo $_POST['Email'] . '<br>';
+  echo $_POST['お問い合わせ内容'];
+} else {
+  echo "HTMLからのPOST送信受信に失敗しました";
+}
